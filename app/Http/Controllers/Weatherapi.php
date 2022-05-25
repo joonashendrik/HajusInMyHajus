e51eeb0fede332c17b8aa51c0c5045983084f0f2
JustKevinR <kevin.reinov@gmail.com>
2022-05-25 09:53:33 +0300

    first commit

--- app/Http/Controllers/Weatherapi.php
+++ app/Http/Controllers/Weatherapi.php
@@ -11,13 +11,10 @@ class Weatherapi extends Controller
 {
     public function index()
     {
-        $apikey = env('API_KEY');
-        $lat = 58.254951;
-        $lon = 22.491898;
         if (Cache::has("weather")) {
             $data = Cache::get('weather');
         } else {
-            $response = Http::get("https://api.openweathermap.org/data/2.5/weather?lat={$lat}&lon={$lon}&appid={$apikey}&units=metric");
+            $response = Http::get('https://api.openweathermap.org/data/2.5/weather?q=kuressaare&units=metric&appid='.config('services.weather.key'));
             Cache::put("weather", array("name" => $response["name"], "sys" => $response["sys"], "wind" => $response["wind"], "weather" => $response["weather"], "main" => $response["main"]),150);
             $data = Cache::get("weather");
         }
